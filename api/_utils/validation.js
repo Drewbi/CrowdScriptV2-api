cadeedeec6bd449fd73e76e266af36c377c1513c
Drew Alexander <drewalexander986@gmail.com>
2020-06-27 21:53:35 +0800

    Quick logic fix for user creation (#10)
    * added vercel to gitignore
    
    * Updated readme (#2)
    
    * Update README.md (#4)
    
    * Change signed url (#6)
    
    * Updated local auth
    
    * Changed to signed url
    
    * Change signed url (#7)
    
    * Updated local auth
    
    * Changed to signed url
    
    * Added key to response
    
    * Fix extention
    
    * Fix: change check to undefined (#9)

--- api/_utils/validation.js
+++ api/_utils/validation.js
@@ -1,5 +1,5 @@
 module.exports.validateFields = (fields) => (req, res, next) => {
-  const invalid = fields.some(field => !req.body[field])
+  const invalid = fields.some(field => req.body[field] === undefined)
   invalid
     ? res.status(400).json({ message: 'Invalid request body' })
     : next()
