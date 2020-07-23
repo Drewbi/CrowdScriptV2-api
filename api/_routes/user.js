2ab368f605ad0c7c5d58edc2e7632f33c43825ce
Drew Alexander <drewalexander986@gmail.com>
2020-07-23 19:11:56 +0800

    Add banning (#14)
    * Add banning
    
    * Add delete session from user

--- api/_routes/user.js
+++ api/_routes/user.js
@@ -3,7 +3,7 @@ const router = Router()
 router.use(json())
 
 const { validateFields } = require('../_utils/validation')
-const { getAllUsers, getUserById, getUserFromJWT, createUser, deleteUser } = require('../_controllers/user')
+const { getAllUsers, getUserById, getUserFromJWT, createUser, banUser, editUser, deleteUser } = require('../_controllers/user')
 const { setUser, verifyUser, verifyAdmin } = require('../_utils/restrict')
 
 router.get('/api/user/current', setUser, verifyUser, getUserFromJWT)
@@ -13,6 +13,10 @@ router.get('/api/user/:id', setUser, verifyAdmin, getUserById)
 router.get('/api/user', setUser, verifyAdmin, getAllUsers)
 
 router.post('/api/user', validateFields(['name', 'email', 'credit', 'password']), createUser)
+
+router.put('/api/user/:id', setUser, verifyAdmin, editUser)
+
+router.put('/api/user/ban/:id', setUser, verifyAdmin, banUser)
 
 router.delete('/api/user', setUser, verifyAdmin, validateFields(['email']), deleteUser)
 
